e27f24fdc1d8d4436ea9cb4d3c5df2d7d1218ef1
trieunt90 <nguyenthanhtrieu90@gmail.com>
2019-03-31 18:32:26 +0700

    up code

--- b5/server/upload.php
+++ b5/server/upload.php
@@ -1,9 +1,27 @@
 <?php 
+// dinh nghia hang so : duong dan upload file
+define('PATH_UPLOAD', 'upload/image/');
+
 if(isset($_POST['btnUpload'])){
 	// kiem tra xem nguoi dung co chon file upload ko
 	if(isset($_FILES['file'])){
 		// kiem tra xem $_FILES se chua cac thong tin cua file gui len
-		echo "<pre>";
-		print_r($_FILES);
+		// echo "<pre>";
+		// print_r($_FILES);
+		$nameFile = $_FILES['file']['name'];
+		$tmpFile = $_FILES['file']['tmp_name'];
+		$erroFile = $_FILES['file']['error'];
+		if($erroFile == 0 && $tmpFile){
+			// file ko co loi va ton tai file can upload o may nguoi dung
+			// tien hanh upload file
+			if(move_uploaded_file($tmpFile, PATH_UPLOAD . $nameFile)){
+				// quay ve lai dung form upload -  voi trang thai thanh cong
+				header("Location:../index4.php?state=success");
+			} else {
+				// quay ve lai dung form upload - voi trang thai that bai
+				header("Location:../index4.php?state=fail");
+			}
+			
+		}
 	}
 }
